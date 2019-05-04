61d01557c4219c286184914d82e6fa76759ac8bc
MiaRene15 <kids.hines@icloud.com>
2019-05-03 22:45:51 -0500

    Update sim.js

--- sim.js
+++ sim.js
@@ -1,6 +1,7 @@
 
   var transition = document.getElementById("transition") // THE ENTIRE CODE OF THE TRANSITION PAGE
   var sim = document.getElementById("sim") // THE ENTIRE CODE OF THE SIMULATION 
+  var end = document.getElementById("end") // THE END SCENE 
   var audio_despondency = new Audio('audio/gratitudeSpace.mp3'); // AUDIO FOR DESPONDENCY 
   var audio_anxiousness = new Audio('audio/tension_releaseSpace.mp3'); // AUDIO FOR ANXIOUSNESS
   var audio_overwhelmed = new Audio('audio/deep_breathingSpace.mp3'); // AUDIO FOR OVERHWHELMED
@@ -13,7 +14,7 @@
     
     if ( what_sim == "despondency") { // IF THE SIMULATION IS THE DESPONDENCY 
       audio_despondency.play(); // PLAY DESPONDENCY AUDIO 
-    
+      
       
     } else if (what_sim == "anxiousness") { // IF THE SIMULATION IS ANXIOUSNESS 
       audio_anxiousness.play(); // PLAY ANXIOUSNESS AUDIO
@@ -21,6 +22,7 @@
       
     } else if (what_sim == "overwhelmed") { // IF THE SIMULATION IS OVERHWHELMED 
       audio_overwhelmed.play(); // PLAY OVERHWHELMED AUDIO
+      setTimeout(function(){end.style.display;},6030;
    
     } else if (what_sim == "stress") { // IF THE SIMULATION IS STRESS 
       audio_stress.play(); //PLAY STRESS AUDIO 
